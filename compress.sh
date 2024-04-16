65b0dd1d37fcd8ab96fdff155f276a0ce3e95f00
Connor Frank <conjfrnk@gmail.com>
2024-04-15 23:17:00 -0400

    diminishing returns gzip

--- compress.sh
+++ compress.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
 
-gzip -9fk style.css
-gzip -9fk *.html
-find assets/ -type f -not -name "*.gz" -exec gzip -5fk "{}" \;
+gzip -5fk style.css
+gzip -7fk *.html
+find assets/ -type f -not -name "*.gz" -exec gzip -4fk "{}" \;
